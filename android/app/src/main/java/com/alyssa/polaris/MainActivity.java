ec5ea0fe9a1fcab4ff7719bc3e869445f6782d4b
Wdxty0419 <1466958298@qq.com>
2026-09-14 15:13:54 +0800

    Update MainActivity.java

--- android/app/src/main/java/com/alyssa/polaris/MainActivity.java
+++ android/app/src/main/java/com/alyssa/polaris/MainActivity.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(SystemFilePlugin.class);
-        registerPlugin(LocalDataSqlitePlugin.class);
-        registerPlugin(NativeProviderHttpPlugin.class);
-        super.onCreate(savedInstanceState);
-    }
+ @Override
+ public void onCreate(Bundle savedInstanceState) {
+ registerPlugin(SystemFilePlugin.class);
+ registerPlugin(LocalDataSqlitePlugin.class);
+ registerPlugin(NativeProviderHttpPlugin.class);
+ super.onCreate(savedInstanceState);
+
+ getBridge().getWebView().setLayerType(android.view.View.LAYER_TYPE_HARDWARE, null);
+ }
 }
